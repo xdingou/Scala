56b391f626672395d7c3f20cfe90f6232060344d
xdingou <dinesh.gouda@gmail.com>
2017-04-07 19:21:01 +0530

    Added methods

--- Scala/src/com/programming/in/scala/chapter6/Rational.scala
+++ Scala/src/com/programming/in/scala/chapter6/Rational.scala
@@ -6,29 +6,29 @@ class Rational(n : Int, d: Int) {
   
   private val gcd: Int = gcd(n, d)
   
-  val numer = n
+  val numer = n / gcd
   
-  val denom = d;
+  val denom = d / gcd;
   
   override def toString = numer + "/" + denom
   
-  private def gcd(x: Int, y: Int) : Int = {
-    if(y==0) x else gcd(y, x% y)
-  }
+  private def gcd(x: Int, y: Int) : Int =  if(y==0) x else gcd(y, x% y)
   
-  def + (that: Rational) :Rational = {
-    new Rational(
-        numer * that.denom + denom * that.numer, denom * that.denom   
-    )  
-  }
+  def + (that: Rational) :Rational = new Rational(numer * that.denom + that.numer * denom, denom * that.denom)  
+
+  def + (i: Int) :Rational = new Rational(numer + i * denom, denom)
   
-  def * (that: Rational) : Rational = {
-    new Rational(
-      numer * that.numer , denom * that.denom    
-    )
-  }
+  def - (that: Rational) : Rational = new Rational(numer*that.denom - that.numer * denom, denom * that.denom)
   
+  def - (i: Int) :Rational = new Rational(numer - i * denom, denom)
+  
+  def * (that: Rational) : Rational = new Rational(numer * that.numer , denom * that.denom)
+
   def * (i: Int) :Rational = new Rational(numer * i, denom)
+  
+  def / (that: Rational) : Rational = new Rational(numer * that.denom, denom * that.numer)
+  
+  def / (i: Int) : Rational = new Rational(numer, denom * i)
   
 }
 
@@ -36,15 +36,19 @@ object Rational {
   def main(args : Array[String]){
     println("I am in ")
     
-    val r = new Rational(1, 2)
+    val r = new Rational(77, 66)
     
-    val q = new Rational(2, 5)   
-   println(r + q)
+    val q = new Rational(6, 3)   
+    
+    val x = new Rational(2, 3)
+    
+    println(x +" * "+ x + " = " + x * x)
+    
+    println(x +" + "+ r + " = " + (x + r))
+    
+    println(x +" + "+ 10 + " = " + (x + 10))
+    
+    println(x +" - "+ r + " = " + (x - r))
    
-   println(r * 10)
-   
-   println(r.gcd)
-   
-   println(q.gcd)
   }
 }
